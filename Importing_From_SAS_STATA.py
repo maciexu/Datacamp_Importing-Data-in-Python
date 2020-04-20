ba7b1fd81aacf31aff20953a3845cff656ef9ddb
maciexu <43970987+maciexu@users.noreply.github.com>
2020-04-19 22:42:28 -0400

    Update Importing_From_SAS_STATA.py

--- Importing_From_SAS_STATA.py
+++ Importing_From_SAS_STATA.py
@@ -14,5 +14,47 @@ Print the head of the DataFrame df_sas.
 Execute your entire script to produce a histogram plot!
 """
 
+# Import sas7bdat package
+from sas7bdat import SAS7BDAT
+
+# Save file to a DataFrame: df_sas
+with SAS7BDAT('sales.sas7bdat') as file:
+    df_sas = file.to_data_frame()
+
+# Print head of DataFrame
+print(df_sas.head())
+
+# Plot histogram of DataFrame features (pandas and pyplot already imported)
+pd.DataFrame.hist(df_sas[['P']])
+plt.ylabel('count')
+plt.show()
+
+"""
+Importing Stata files
+
+Here, you'll gain expertise in importing Stata files as DataFrames using the pd.read_stata() function from pandas. The last exercise's file, 'disarea.dta', is still in your working directory.
+"""
+
+# Import pandas
+import pandas as pd
+
+# Load Stata file into a pandas DataFrame: df
+df = pd.read_stata('disarea.dta')
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Plot histogram of one column of the DataFrame
+pd.DataFrame.hist(df[['disa10']])
+plt.xlabel('Extent of disease')
+plt.ylabel('Number of countries')
+plt.show()
+
+
+
+
+
+
+
 
 
